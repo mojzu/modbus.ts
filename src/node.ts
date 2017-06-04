c456db462fdc37ffb8b362202bfb89fd0f4feee8
mojzu <mail@mojzu.net>
2017-06-04 22:53:13 +0100

    chore: move Node.js dependent imports to own module

---
+++ src/node.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+import * as assert from "assert";
+export { assert };
+import { Buffer } from "buffer";
+export { Buffer };
+import { Socket, createConnection } from "net";
+export { Socket, createConnection };
